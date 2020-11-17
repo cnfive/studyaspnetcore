9f8f4ed8a5b0bfb70b5e6801d10c8112cb64b6d8
yang <37206103+cnfive@users.noreply.github.com>
2020-11-17 17:27:16 +0800

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,179 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using WebApplication16.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
+namespace WebApplication16
+{
+    public static class RolesData
+    {
+        private static readonly string[] Roles = new string[] { "Administrator", "Editor", "Subscriber" };
+
+        public static async Task SeedRoles(IServiceProvider serviceProvider)
+        {
+            using (var serviceScope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var dbContext = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+
+                if (dbContext.Database.GetPendingMigrations().Any())
+                {
+                    await dbContext.Database.MigrateAsync();
+
+                    var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                    foreach (var role in Roles)
+                    {
+                        if (!await roleManager.RoleExistsAsync(role))
+                        {
+                            await roleManager.CreateAsync(new IdentityRole(role));
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+            services.AddSingleton(HtmlEncoder.Create(UnicodeRanges.All));
+            // services.AddIdentity<IdentityUser, IdentityRole>();
+
+            // session 设置
+            services.AddSession(options =>
+            {
+                // 设置 Session 过期时间
+                options.IdleTimeout = TimeSpan.FromDays(90);
+               // options.CookieHttpOnly = true;
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            // 要添加在MVC服务前面
+            app.UseSession();
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseCookiePolicy();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+             CreateRoles(serviceProvider).Wait();
+           // RolesData.SeedRoles(app.ApplicationServices).Wait();
+
+        }
+
+        private async Task CreateRoles(IServiceProvider serviceProvider)
+        {
+            //initializing custom roles   
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            string[] roleNames = { "Admin", "User", "HR" };
+            IdentityResult roleResult;
+
+            foreach (var roleName in roleNames)
+            {
+                var roleExist = await RoleManager.RoleExistsAsync(roleName);
+                if (!roleExist)
+                {
+                    //create the roles and seed them to the database: Question 1  
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+            IdentityUser user = await UserManager.FindByEmailAsync("jignesh@gmail.com");
+
+            if (user == null)
+            {
+                user = new IdentityUser()
+                {
+                    UserName = "jignesh@gmail.com",
+                    Email = "jignesh@gmail.com",
+                };
+                await UserManager.CreateAsync(user, "Test@123");
+            }
+            await UserManager.AddToRoleAsync(user, "Admin");
+
+
+            IdentityUser user1 = await UserManager.FindByEmailAsync("tejas@gmail.com");
+
+            if (user1 == null)
+            {
+                user1 = new IdentityUser()
+                {
+                    UserName = "tejas@gmail.com",
+                    Email = "tejas@gmail.com",
+                };
+                await UserManager.CreateAsync(user1, "Test@123");
+            }
+            await UserManager.AddToRoleAsync(user1, "User");
+
+            IdentityUser user2 = await UserManager.FindByEmailAsync("rakesh@gmail.com");
+
+            if (user2 == null)
+            {
+                user2 = new IdentityUser()
+                {
+                    UserName = "rakesh@gmail.com",
+                    Email = "rakesh@gmail.com",
+                };
+                await UserManager.CreateAsync(user2, "Test@123");
+            }
+            await UserManager.AddToRoleAsync(user2, "HR");
+
+        }
+
+    }
+}
