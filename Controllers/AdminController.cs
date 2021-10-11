38650d91755175b8b1f8453ba45be05fcae1ba98
yang <37206103+cnfive@users.noreply.github.com>
2021-10-11 15:49:28 +0800

    Add files via upload

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using WebApplication16.Models;
+
+
+namespace WebApplication16.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<IdentityUser> userManager;
+        public AdminController(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
+        {
+            this.roleManager = roleManager;
+            this.userManager = userManager;
+
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //我们只需要指定一个不重复的角色名称来创建新角色
+
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = model.RoleName
+                };
+
+                //将角色保存在AspNetRoles表中
+
+                IdentityResult result = await roleManager.CreateAsync(identityRole);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles", "Admin");
+                }
+
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication16.Models;
+
+using System.Net.Http.Headers;
+using System.IO;
+
+
+
+namespace WebApplication16.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+
+        [HttpPost]
+
+
+
+        public JsonResult UploadCKEditorImage()
+
+
+
+        {
+
+
+
+            var files = Request.Form.Files;
+
+
+
+            if (files.Count == 0)
+
+
+
+
+            {
+
+
+
+
+                var rError = new
+
+
+
+
+                {
+
+
+
+
+                    uploaded = false,
+
+
+
+
+                    url = string.Empty
+
+
+
+
+                };
+
+
+
+
+                return Json(rError);
+
+
+
+
+            }
+
+
+
+
+            var formFile = files[0];
+
+
+
+
+            var upFileName = formFile.FileName;
+
+
+
+
+            //大小，格式校验....
+
+
+
+
+            var fileName = Guid.NewGuid() + Path.GetExtension(upFileName);
+
+
+
+
+            var saveDir = @".\wwwroot\upload\";
+
+
+
+
+            var savePath = saveDir + fileName;
+
+
+
+
+            var previewPath = "/upload/" + fileName;
+
+
+
+
+
+
+
+
+
+            bool result = true;
+
+
+
+
+            try
+
+
+
+
+            {
+
+
+
+
+                if (!Directory.Exists(saveDir))
+
+
+
+
+                {
+
+
+
+
+                    Directory.CreateDirectory(saveDir);
+
+
+
+
+                }
+
+
+
+
+                using (FileStream fs = System.IO.File.Create(savePath))
+
+
+
+
+                {
+
+
+
+
+                    formFile.CopyTo(fs);
+
+
+
+
+                    fs.Flush();
+
+
+
+
+                }
+
+
+
+
+            }
+
+
+
+
+            catch (Exception ex)
+
+
+
+
+            {
+
+
+
+
+                result = false;
+
+
+
+
+            }
+
+
+
+
+            var rUpload = new
+
+
+
+
+            {
+
+
+
+
+                uploaded = result,
+
+
+
+
+                url = result ? previewPath : string.Empty
+
+
+
+
+            };
+
+
+
+
+            return Json(rUpload);
+
+
+
+
+        }
+
+
+
+    }
+}
